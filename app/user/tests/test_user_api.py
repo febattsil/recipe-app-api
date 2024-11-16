0228df72641eacdd16c13d58ecc7ed03c6444544
Fernando Battisti da Silva <febattisti@outlook.com>
2024-11-15 21:41:47 -0300

    updated features

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -42,7 +42,7 @@ class PublicUserApiTests(TestCase):
 
 
     def test_user_with_email_exists_erros(self):
-        """Test error returned if user ith email exists. """
+        """Test error returned if user with email exists. """
         payload={
             'email' : 'test@example.com',
             'password' : 'testpass123',
@@ -93,7 +93,7 @@ class PublicUserApiTests(TestCase):
         payload = {'email': 'test@example.com', 'password': 'badpass'}
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertIn('token', res.data)
+        self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
@@ -101,7 +101,7 @@ class PublicUserApiTests(TestCase):
         payload = {'email': 'test@example.com', 'password': '',}
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertIn('token', res.data)
+        self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_retrieve_user_unauthorized(self):
